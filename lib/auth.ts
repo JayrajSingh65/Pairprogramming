d4d550d25f271c180026d3ddf11215a430da5d64
jayrajsingh65 <singhjayraj69@gmail.com>
2024-09-28 21:34:08 +0530

    new change for github

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,6 +4,7 @@ import { AuthOptions, DefaultSession, getServerSession } from "next-auth";
 import { Adapter } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google";
 import DiscordProvider from "next-auth/providers/discord";
+import GitHubProvider from "next-auth/providers/github";
 
 declare module "next-auth" {
   interface Session extends DefaultSession {
@@ -26,6 +27,10 @@ export const authConfig = {
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID!,
       clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
     })
   ],
   callbacks: {
